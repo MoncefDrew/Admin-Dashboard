98ac29546104f332c38ed49bdd4896f91fc6fd74
mokranimoncef <moncef.mokrani@etu.univ-batna2.dz>
2024-09-29 22:24:40 +0100

    sidebar being top of the nav bar problem

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import {ColorModeContext,useMode} from "./theme";
 import {CssBaseline,ThemeProvider} from "@mui/material";
-import topbar from "./scenes/Global/Topbar";
 import Topbar from "./scenes/Global/Topbar";
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "./scenes/Dashboard";
+import Sidebar from "./scenes/Global/sidebar";
+/*import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
+import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import FAQ from "./scenes/faq";
+import Calendar from "./scenes/calendar";
+import Geography from "./scenes/geography";
+*/
+
 
 
 function App() {
   const [theme, colorMode] = useMode();
   return (
       //this is the way to set up the color context
-      //so we have its access everywhere inside the app
+      //,so we have its access everywhere inside the app
       <ColorModeContext.Provider value={colorMode}>
         <ThemeProvider theme={theme}>
           <CssBaseline/>
           <div className="App">
+            <Sidebar/>
             <main className="content">
-              <Topbar></Topbar>
+              <Topbar/>
+              <Routes>
+                    <Route path="/" element={<Dashboard/>}/>
+                    {/*<Route path="/team" element={<Team/>}/>
+                    <Route path="/contacts" element={<Contacts/>}/>
+                    <Route path="/invoices" element={<Invoices/>}/>
+                    <Route path="/form" element={<Form/>}/>
+                    <Route path="/bar" element={<Bar/>}/>
+                    <Route path="/pie" element={<Pie/>}/>
+                    <Route path="/line" element={<Line/>}/>
+                    <Route path="/faq" element={<FAQ/>}/>
+                    <Route path="/geography element={<Geography/>
+                    <Route path="/calendar element={<Calendar/>}"
+                    */}
+
+                </Routes>
             </main>
           </div>
         </ThemeProvider>
--- src/scenes/Global/sidebar.jsx
+++ src/scenes/Global/sidebar.jsx
@@ -1,6 +1,82 @@
+import {useState} from "react";
+import { Menu ,MenuItem, ProSidebar} from "react-pro-sidebar";
+ import {Box, IconButton,Typography ,useTheme} from "@mui/material";
+import links from "react-router-dom";
+import {tokens} from "../../theme";
+import "react-pro-sidebar/dist/css/styles.css";
+
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+//import CalendarOutlinedIcon from '@mui/icons-material/CalendarOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarchartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+//import TimeLineOutlinedIcon from '@mui/icons-material/TimeLineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
 
-const sidebar = ( ) =>{
-    return (<div>sidebar</div>)
+const Sidebar = ( ) =>{
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const theme = useTheme();
+    const colors= tokens(theme.palette.mode);
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isCollapsed, setisCollapsed] = useState(false);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [Selected, setSelected] = useState("Dashboard");
+    return (
+        <Box 
+            sx={{
+                "& .pro-sidebar-inner":{
+                    background : `${colors.primary[400]} !important`,
+                },
+                "& .pro-icon-wrapper":{
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item":{
+                    padding:"5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover":{
+                    color: "#868dfb !important"
+                },
+                "& .pro-menu-item.active":{
+                    color:"#6870fa !important"
+                },
+            }}
+
+        >
+            <ProSidebar collapsed={isCollapsed} >
+                <Menu iconShape="square">
+                    {/* LGO AND MENU ICON */}
+                    <MenuItem
+                        onClick={() => setisCollapsed(!isCollapsed)}
+                        icon={isCollapsed ? <MenuOutlinedIcon/> : undefined}
+                        style={{
+                            margin: "10px 0 20px 0",
+                            color: colors.grey[100],
+                        }}>
+                        {!isCollapsed &&(
+                            <Box display="flex"
+                                 justifyContent="space-between"
+                                 alignItems="center"
+                                 ml="15px">
+                                <Typography>
+                                    ADMINIS
+                                </Typography>
+                                <IconButton>
+                                    <MenuOutlinedIcon/>
+                                </IconButton>
+                            </Box>
+                        )}
+                    </MenuItem>
+                </Menu>
+            </ProSidebar>
+        </Box>
+
+    )
 }
-export default sidebar;
+export default Sidebar;
