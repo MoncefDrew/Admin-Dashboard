91f81136682f85d91c7151455d7f20a4f83b841c
mokranimoncef <moncef.mokrani@etu.univ-batna2.dz>
2024-10-03 23:26:23 +0100

    i resolved the sidebar problem being on top of the navigation bar by changing css classes (making a flex row direction for the App class so the sidebar and the nav bar will be on the same row) and then i made the menu items aligned vertically inside the prosidebar

--- src/scenes/Global/sidebar.jsx
+++ src/scenes/Global/sidebar.jsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import { Menu ,MenuItem, ProSidebar} from "react-pro-sidebar";
  import {Box, IconButton,Typography ,useTheme} from "@mui/material";
-import links from "react-router-dom";
+import links, {Link} from "react-router-dom";
 import {tokens} from "../../theme";
 
 import "react-pro-sidebar/dist/css/styles.css";
@@ -10,14 +10,28 @@ import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
 import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
 import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-//import CalendarOutlinedIcon from '@mui/icons-material/CalendarOutlined';
 import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
 import BarchartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
 import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
-//import TimeLineOutlinedIcon from '@mui/icons-material/TimeLineOutlined';
+import TimeLineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import {CalendarTodayOutlined} from "@mui/icons-material";
 
+const Item =  ({title, to, icon, Selected,setSelected}) =>{
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return(
+        <MenuItem active={Selected === title}
+                  style={{color : colors.grey[100]}}
+                  onClick={()=> setSelected(title)}
+                  icon={icon}>
+
+            <Typography> {title} </Typography>
+            <Link to={to}/>
+        </MenuItem>
+    )
+}
 
 const Sidebar = ( ) =>{
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -71,7 +85,9 @@ const Sidebar = ( ) =>{
                                 alignItems="center"
                                 ml="15px"
                             >
-                                <Typography>ADMINIS</Typography>
+                                <Typography variant="h3" color={colors.grey[100]}>
+                                    ADMINIS
+                                </Typography>
                                 <IconButton onClick={() => setisCollapsed(!isCollapsed)}>
                                     <MenuOutlinedIcon />
                                 </IconButton>
@@ -79,13 +95,105 @@ const Sidebar = ( ) =>{
                         )}
                     </MenuItem>
 
+                    {!isCollapsed && (
+                        <Box mb="25px">
+                            <Box display="flex" justifyContent="center" alignItems="center">
+                                <img
+                                    alt="profile-user"
+                                    width="100px"
+                                    height="100px"
+                                    src={`../../assets/user.png`}
+                                    style={{ cursor: "pointer", borderRadius: "50%" }}
+                                />
+                            </Box>
+                            <Box textAlign="center">
+                                <Typography
+                                    variant="h2"
+                                    color={colors.grey[100]}
+                                    fontWeight="bold"
+                                    sx={{ m: "10px 0 0 0" }}
+                                >
+                                    Ed Roh
+                                </Typography>
+                                <Typography variant="h5" color={colors.greenAccent[500]}>
+                                    VP Fancy Admin
+                                </Typography>
+                            </Box>
+                        </Box>
+                    )}
+
+
                     {/* Menu Items (Now Vertically Aligned) */}
-                    <MenuItem icon={<HomeOutlinedIcon />}>Dashboard</MenuItem>
-                    <MenuItem icon={<PeopleOutlinedIcon />}>Users</MenuItem>
-                    <MenuItem icon={<ContactsOutlinedIcon />}>Contacts</MenuItem>
-                    <MenuItem icon={<ReceiptOutlinedIcon />}>Invoices</MenuItem>
-                    <MenuItem icon={<PersonOutlinedIcon />}>Profile</MenuItem>
-                    <MenuItem icon={<HelpOutlinedIcon />}>Help</MenuItem>
+                    <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+                        <Item title="Dashboard"
+                              to="/"
+                              icon={<HomeOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Manage Team"
+                              to="/team"
+                              icon={<PeopleOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Contacts Information"
+                              to="/contacts"
+                              icon={<ContactsOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Invoices Balances"
+                              to="/invoices"
+                              icon={<ReceiptOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Profile Form"
+                              to="/form"
+                              icon={<PersonOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Calendar"
+                              to="/calendar"
+                              icon={<CalendarTodayOutlined/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="FAQ Page"
+                              to="/faq"
+                              icon={<HomeOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Bar Chart"
+                              to="/bar"
+                              icon={<BarchartOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Pie Chart"
+                              to="/pie"
+                              icon={<PieChartOutlineOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Line Chart"
+                              to="/line"
+                              icon={<TimeLineOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+                        <Item title="Geography Chart"
+                              to="/geography"
+                              icon={<MapOutlinedIcon/>}
+                              setSelected={setSelected}
+                              selected={Selected}
+                        />
+
+
+                    </Box>
                 </Menu>
             </ProSidebar>
 
